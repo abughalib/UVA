32bad1e6c46681eb9f40e738ceb6973ec8e10fba
Abu Ghalib <33085535+abughalib@users.noreply.github.com>
2020-04-13 10:09:11 +0530

    Create lastStoneRecursive.cpp

---
+++ lastStoneRecursive.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        if(stones.size() == 0) return 0;
+        else if(stones.size() == 1) return stones[0];
+        else{
+        	sort(stones.begin(), stones.end());
+        	stones[stones.size()-2] = abs(stones[stones.size()-1]-stones[stones.size()-2]);
+        	stones.pop_back();
+        	
+        }
+        return lastStoneWeight(stones);
+    }
+};
+
+int main(){
+
+	vector<int> vec={2,7,4,1,8,1};
+
+	Solution sol;
+	cout <<sol.lastStoneWeight(vec);
+}
